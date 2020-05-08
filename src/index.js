29ac1a8a95b514606d1c7058a224ec03e48856bf
Francesca Dreith <francescadreith@Francescas-MacBook-Air.local>
2020-05-07 19:35:50 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom'
-import App from './App'
+import ReactDOM from 'react-dom';
+import App from './App';
 
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk'
+import catsReducer from './reducers/catsReducer.js';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSTION_COMPOSE__ || compose
+
+const store = createStore(catsReducer, composeEnhancers(applyMiddleware(thunk)))
 
 ReactDOM.render(
-    <App />,
+    <Provider store={store}>
+        <App />
+    </Provider>,
     document.getElementById('root')
 );
