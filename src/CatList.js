5dabef9d7d760dc40f9e7d7a46670ab5235c9d6f
Noah Scala <nwscala@gmail.com>
2020-05-08 17:20:36 -0400

    Done.

--- src/CatList.js
+++ src/CatList.js
@@ -1,1 +1,16 @@
 // write your CatList component here
+import React from 'react'
+
+export default function CatList(props) {
+    const renderCats = () => {
+        return props.catPics.map((catPic, index) => {
+            return <img key={index} src={catPic.url}></img>
+        })
+    }
+    
+    return (
+        <div>
+            {renderCats()}
+        </div>
+    )
+}
