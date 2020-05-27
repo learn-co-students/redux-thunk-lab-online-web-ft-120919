022204516a60e853ea6df2c099820ed55133a8cc
mrarthurwhite <mister.arthur.white@gmail.com>
2020-05-27 01:50:21 -0400

    Done.

---
+++ src/Appv1.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class App extends Component {   
+  
+  render() {
+    return (
+      <div>
+        <h1>CatBook</h1>
+        {/* add CatList component here */}
+      </div>
+    );
+  }
+}
+
+export default App
+
